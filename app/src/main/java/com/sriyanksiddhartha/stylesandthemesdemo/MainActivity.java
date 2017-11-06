14b121759e4f10c3545f2a880440d4ad2af4629e
Harsh Doshi <dosh0005@algonquinlive.com>
2017-11-06 17:59:31 -0500

    Style demo complete.

--- app/src/main/java/com/sriyanksiddhartha/stylesandthemesdemo/MainActivity.java
+++ app/src/main/java/com/sriyanksiddhartha/stylesandthemesdemo/MainActivity.java
@@ -3,31 +3,20 @@ package com.sriyanksiddhartha.stylesandthemesdemo;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Shader;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.widget.TextView;
 
-
-/** 
- * Author : Sriyank Siddhartha 
- *
- * Module 2 : Styling Views
- *  
- * 		"AFTER" demo project
- **/
+/**
+ * Author: Harsh Doshi (dosh0005)
+ * */
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);  // Change the layout to "activity_button" to check button applied styles
+		setContentView(R.layout.activity_main);
 
-		/*
-		* Comment the below code :
-		* 	1. First by making sure we have "setContentView(R.layout.activity_button)"
-		* 	2. To check for the styles applied to Button
-		*   3. To avoid NULL POINTER EXCEPTION
-		* */
 		TextView txvGradient = (TextView) findViewById(R.id.txvGradient);
 		LinearGradient linearGradient = new LinearGradient(0, 0,
 				0, txvGradient.getTextSize(), Color.GREEN, Color.RED, Shader.TileMode.MIRROR);
